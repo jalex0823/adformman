7265cb023c72e40e11c5521af088e0f49929a755
jalex0823 <91576032+jalex0823@users.noreply.github.com>
2023-11-05 20:23:36 -0600

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sqltps1:Disneychannel911!@sql01-tps-dev-scus.database.windows.net/tps_aggroupdb'
+db = SQLAlchemy(app)
+
+class Personnel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    role = db.Column(db.String(50))
+
+@app.route('/personnel')
+def personnel():
+    personnel = Personnel.query.all()
+    return render_template('personnel.html', personnel=personnel)
+
+@app.route('/update_role', methods=['POST'])
+def update_role():
+    personnel_id = request.form.get('personnel_id')
+    new_role = request.form.get('new_role')
+    personnel = Personnel.query.get(personnel_id)
+    personnel.role = new_role
+    db.session.commit()
+    return redirect(url_for('personnel'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
