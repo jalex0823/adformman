41ef690cb4e26b531d523cc2740dba86577b34c4
jalex0823 <91576032+jalex0823@users.noreply.github.com>
2023-11-10 12:59:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask, request, render_template
-from flask_mail import Mail, Message
 import os
-import traceback
 from flask_sqlalchemy import SQLAlchemy
-import werkzeug.exceptions
 
 app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@example.com'  # replace with your email
-app.config['MAIL_PASSWORD'] = 'Disneychannel911!'  # replace with your password
-mail = Mail(app)
 db = SQLAlchemy(app)
+
 class ITDepartments(db.Model):
     __tablename__ = 'ITDepartments'
     Department = db.Column(db.String(255), primary_key=True)
@@ -36,29 +28,19 @@ def home():
     personnel = ITPersonnel.query.all()
     departments = ITDepartments.query.all()
     return render_template('index.html', personnel=personnel, departments=departments)
+
 @app.route('/assign-rights', methods=['POST'])
 def assign_rights():
     for person in ITPersonnel.query.all():
         last_name = person.Last_Name
         privileges = request.form.get(f'privileges_{last_name}')
         remarks = request.form.get(f'remarks_{last_name}')
-        if privileges:
-            person.Database_Privileges = privileges
+        if privileges: person.Database_Privileges = privileges
         if remarks:
             person.Remarks = remarks
     db.session.commit()
 
-    # Send email notification
-    send_email()
-
     return 'Rights assigned successfully'
-
-def send_email():
-    msg = Message('Rights Assigned',
-                  sender='genev@texaspipe.com',  # replace with your email
-                  recipients=['jefferya@texaspipe.com'])
-    msg.body = 'The rights have been successfully assigned.'
-    mail.send(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
